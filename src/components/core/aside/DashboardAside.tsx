52737dc8810f601b961acb943907cf1d9edc2513
Sayemsaadat0 <sayemsaadat0@gmail.com>
2024-10-29 22:54:47 +0600

    changes

--- src/components/core/aside/DashboardAside.tsx
+++ src/components/core/aside/DashboardAside.tsx
@@ -21,13 +21,38 @@ const DashboardAside: FC = () => {
             label: "Dashboard",
             icon: <DashboardIcon size={'24'} />,
         },
+        {
+            path: "/admin/article",
+            label: "Article",
+            icon: <DashboardIcon size={'24'} />,
+        },
+        {
+            path: "/admin/stats",
+            label: "Stats",
+            icon: <DashboardIcon size={'24'} />,
+        },
+        {
+            path: "/admin/photos",
+            label: "Photos",
+            icon: <DashboardIcon size={'24'} />,
+        },
+        {
+            path: "/admin/players-info",
+            label: "Players Info",
+            icon: <DashboardIcon size={'24'} />,
+        },
+        {
+            path: "/admin/users",
+            label: "Users",
+            icon: <DashboardIcon size={'24'} />,
+        },
     ];
 
     return (
-        <div className="h-screen p-5 bg-oc-primary-1-900 max-w-[280px] px-10 flex flex-col justify-between">
+        <div className="h-screen p-5 bg-gradient-to-t from-c-violet-500 via-c-violet-700 to-c-violet-900 max-w-[280px] px-10 flex flex-col justify-between">
             <div className="space-y-20">
                 <div className="flex items-center gap-3">
-                    <Logo isAdmin />
+                    <Logo  />
                 </div>
                 <div className="text-oc-white-50">
                     <div className="space-y-3">
@@ -35,7 +60,7 @@ const DashboardAside: FC = () => {
                             <Link
                                 key={index}
                                 href={item.path}
-                                className={`flex gap-2 items-center w-full py-2 transition-all px-2 rounded-[6px] ${pathname.startsWith(item.path) ? "bg-oc-white-50 text-oc-primary-1-900" : "hover:bg-oc-white-50 hover:text-oc-primary-1-900"}`}
+                                className={`flex gap-2 items-center w-full py-2 transition-all px-2 rounded-[6px] ${pathname.startsWith(item.path) ? " bg-c-violet-50 " : "hover:bg-c-violet-50 hover:text-black  text-c-violet-50 "}`}
                             >
                                 {item.icon}
                                 {item.label}
--- src/components/core/navbar/DashboardTopNavbar.tsx
+++ src/components/core/navbar/DashboardTopNavbar.tsx
@@ -51,17 +51,17 @@ const DashboardTopNavbar: FC = () => {
     const currentMenuItem = menuItems.find(item => pathname.startsWith(item.path));
 
     return (
-        <div className="px-8 bg-oc-white-500 py-6 border-b border-b-oc-primary-1-900 flex justify-between">
+        <div className="px-8 bg-c-white-200 py-6 border-b  flex justify-between">
             <div className="flex gap-4 items-center">
                 <div className="flex gap-2">
                     <div
                         onClick={back}
-                        className="p-4 bg-oc-white-600/60 border cursor-pointer border-oc-primary-1-900 rounded-full hover:bg-oc-primary-1-900 hover:text-oc-primary-1-50"
+                        className="p-4 transition-all border cursor-pointer hover:bg-c-violet-500 hover:text-c-violet-50 rounded-full "
                     >
                         <ArrowIcon className="rotate-180" />
                     </div>
                     <div
-                        className="p-4 bg-oc-white-600/60 cursor-pointer border border-oc-primary-1-900 rounded-full hover:bg-oc-primary-1-900 hover:text-oc-primary-1-50"
+                        className="p-4 transition-all cursor-pointer border hover:bg-c-violet-500 hover:text-c-violet-50 rounded-full "
                         onClick={forward}
                     >
                         <ArrowIcon />
@@ -70,13 +70,13 @@ const DashboardTopNavbar: FC = () => {
                 <div>
                     <p className="leading-none text-w-title-1-Medium-22">Admin Dashboard</p>
                     {/* Display the label based on the current pathname */}
-                    <p className="text-w-paragraph-regular-20 text-oc-white-700">
+                    <p className="text-w-paragraph-regular-20 ">
                         {currentMenuItem ? currentMenuItem.label : 'Dashboard'}
                     </p>
                 </div>
             </div>
 
-            <div className='flex text-w-paragraph-regular-20 cursor-pointer hover:text-oc-primary-1-800 items-center  gap-2'>
+            <div className='flex text-w-paragraph-regular-20 cursor-pointer  items-center  gap-2'>
                 <SignoutIcon size={'24'} /> Sign out
             </div>
         </div>
