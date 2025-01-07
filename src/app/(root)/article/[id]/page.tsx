bfbe509f04cd669603ab376db627124f962cf5d2
Sabbir Mohammad Sami <smd71430@gmail.com>
2025-01-07 17:44:55 +0600

    show stats detials

--- src/app/(root)/article/[id]/page.tsx
+++ src/app/(root)/article/[id]/page.tsx
@@ -1,4 +1,3 @@
-
 // import RandomShapeGenerator from '@/components/core/RandomShapeGenerator'
 // import React from 'react'
 // import ArticleDetailsContainer from './_components/ArticleDetailsContainer'
@@ -18,8 +17,6 @@
 
 // export default page
 
-
-
 // import BlogDetailsContainer from "@/components/page/blog/blog-details/BlogDetailsContainer";
 
 // import axiosRequest from "@/lib/axiosRequest";
@@ -29,7 +26,6 @@
 // interface ArticleDetailsType {
 //     params: { id: number };
 // }
-
 
 // const ArticleDetails: FC<ArticleDetailsType> = async ({ params }) => {
 
@@ -57,44 +53,43 @@
 
 // export default ArticleDetails;
 
-import React from "react";
+import RandomShapeGenerator from "@/components/core/RandomShapeGenerator";
 import axiosRequest from "@/lib/axiosRequest";
 import ArticleDetailsContainer from "./_components/ArticleDetailsContainer";
-import RandomShapeGenerator from "@/components/core/RandomShapeGenerator";
 
 interface Props {
-    params: { id: number };
+  params: { id: number };
 }
 
 const page = async ({ params }: Props) => {
-    const { id } = params;
+  const { id } = params;
 
-    console.log(id)
-    try {
-        const data = await axiosRequest({
-            url: `/article/${id}/`,
-            method: "get",
-        });
-        console.log("Fetched data:", data);
+  console.log(id);
+  try {
+    const data = await axiosRequest({
+      url: `/article/${id}/`,
+      method: "get",
+    });
+    console.log("Fetched data:", data);
 
-        return (
-            <div className="crick-Container relative  ">
-                <div className='mt-10 max-w-7xl overflow-hidden mx-auto fixed  w-full top-0 left-1/2 -translate-x-1/2'>
-                    <RandomShapeGenerator />
-                </div>
-                <div className='relative mx-auto mt-24  w-[80%]'>
-                    <ArticleDetailsContainer data={data} />
-                </div>
-            </div>
-        );
-    } catch (error) {
-        console.error("Error fetching article details:", error);
-        return (
-            <div>
-                <p>Failed to load article details. Please try again later.</p>
-            </div>
-        );
-    }
+    return (
+      <div className="crick-Container relative  ">
+        <div className="mt-10 max-w-7xl overflow-hidden mx-auto fixed  w-full top-0 left-1/2 -translate-x-1/2">
+          <RandomShapeGenerator />
+        </div>
+        <div className="relative mx-auto mt-24  w-[80%]">
+          <ArticleDetailsContainer data={data} />
+        </div>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching article details:", error);
+    return (
+      <div>
+        <p>Failed to load article details. Please try again later.</p>
+      </div>
+    );
+  }
 };
 
 export default page;
--- src/app/(root)/stats/[slug]/page.tsx
+++ src/app/(root)/stats/[slug]/page.tsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import StatsDetailsContainer from '../_components/_details/StatsDetailsContainer'
+import RandomShapeGenerator from "@/components/core/RandomShapeGenerator";
+import axiosRequest from "@/lib/axiosRequest";
+import StatsDetailsContainer from "../_components/_details/StatsDetailsContainer";
 
-const page = () => {
-  return (
-    <div>
-        <StatsDetailsContainer />
-    </div>
-  )
+interface Props {
+  params: { slug: number };
 }
 
-export default page
+const page = async ({ params }: Props) => {
+  const { slug } = params;
+
+  console.log(slug);
+  try {
+    const data = await axiosRequest({
+      url: `/stats/${slug}/`,
+      method: "get",
+    });
+    console.log("Fetched data:", data);
+
+    return (
+      <div className="crick-Container relative  ">
+        <div className="mt-10 max-w-7xl overflow-hidden mx-auto fixed  w-full top-0 left-1/2 -translate-x-1/2">
+          <RandomShapeGenerator />
+        </div>
+        <div className="relative mx-auto mt-24  w-[80%]">
+          <StatsDetailsContainer data={data} />
+        </div>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching article details:", error);
+    return (
+      <div>
+        <p>Failed to load article details. Please try again later.</p>
+      </div>
+    );
+  }
+};
+
+export default page;
